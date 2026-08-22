9a9afe83e62845773b04e954751c68ab640fcd77
Zhi B Gao <gaobensin@gmail.com>
2026-08-21 22:12:46 -0500

    add a 60 FPS system to the game

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((sw, sh))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {sw}")
@@ -14,6 +16,7 @@ def main():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
