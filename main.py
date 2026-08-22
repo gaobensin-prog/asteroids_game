fd16bc87289cecbcee40e44457b5749a85a5b7cf
Zhi B Gao <gaobensin@gmail.com>
2026-08-21 21:59:49 -0500

    Set up a black screen with pygame display with the method .fill() to fill the screen and method .flip() to update the screen inside the loop. Had to update wsl in order to actually display the screen.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
-from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+from constants import SCREEN_WIDTH as sw, SCREEN_HEIGHT as sh
+from logger import log_state
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((sw, sh))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {sw}")
+    print(f"Screen height: {sh}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
