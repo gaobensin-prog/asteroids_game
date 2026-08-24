8ef37050026caeb3471083c87c899c095485ab65
Zhi B Gao <gaobensin@gmail.com>
2026-08-24 11:36:50 -0500

    add a asteroid class and copy the asteroidfield code to actually spawn the asteroid, then spend 30 minutes figuring out why the asteroid was being generated yes but it was staying in one spot and not moving around, come to find out in the Asteroid class with the draw() method one of the argument was a staic not value of (0,0) when it suppose to be the parent class (CircleShape) object's self.position that was mofity by the Asteroid class update().

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import pygame
 from constants import SCREEN_WIDTH as sw, SCREEN_HEIGHT as sh
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    field=AsteroidField()
     player=Player(sw/2, sh/2)
     pygame.init()
     clock = pygame.time.Clock()
