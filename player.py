23f56fce40ae8d92b0c95590e433d1f405dd77fb
Zhi B Gao <gaobensin@gmail.com>
2026-08-23 19:56:54 -0500

    add a move left and move right freature

--- player.py
+++ player.py
@@ -1,10 +1,19 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS as pr
+from constants import PLAYER_RADIUS as pr, PLAYER_TURN_SPEED as pts
 import pygame
 class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,radius = pr)
         self.rotation = 0
+    def rotate(self,dt):
+        self.rotation += pts * dt
+    def update(self,dt:float):
+        keys = pygae.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation)
