d63b0e165140ee76ca5b96f729f56d19317a51e6
Zhi B Gao <gaobensin@gmail.com>
2026-08-23 20:32:14 -0500

    add the freate to move up and down

--- player.py
+++ player.py
@@ -14,6 +14,15 @@ class Player(CircleShape):
             self.rotate(dt)
         if keys[pygame.K_d]:
             self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(0)
+        if keys[pygame.K_w]:
+            self.move(dt)
+    def move(self,dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation)
