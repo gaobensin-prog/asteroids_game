e067b864c6e1b7cd57fc347bee82fc77e88a5de6
Zhi B Gao <gaobensin@gmail.com>
2026-08-23 19:58:16 -0500

    added the player.update()

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
             pass
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
