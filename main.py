ca56281a630de563aa9560f7681a7f6c8fc2c1e6
Zhi B Gao <gaobensin@gmail.com>
2026-08-24 20:46:32 -0500

    add the projective

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 import sys
 def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots,drawable,updatable)
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
@@ -33,6 +36,7 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS as pr, PLAYER_TURN_SPEED as pts,PLAYER_SPEED
+from constants import PLAYER_RADIUS as pr, PLAYER_TURN_SPEED as pts,PLAYER_SPEED,PLAYER_SHOOT_SPEED
 import pygame
+from shot import Shot
 class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,radius = pr)
@@ -18,6 +19,8 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     def move(self,dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
@@ -31,3 +34,6 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    def shoot(self):
+        shot=Shot(self.position[0],self.position[1])
+        shot.velocity = pygame.math.Vector2.rotate(pygame.Vector2(0,1),self.rotation) * PLAYER_SHOOT_SPEED
